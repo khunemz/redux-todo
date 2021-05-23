df5133dbfcc18d8bb5d0cb155eb0a2ca48189d27
khunemz <khunemz@gmail.com>
2021-05-23 21:46:50 +0700

    add card

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,39 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
+import { useDispatch , useSelector } from 'react-redux'
+import {Card, CardActions, CardContent,Typography, Button} from '@material-ui/core';
+
+import { getTodos } from './../actions/todoActions';
 
 const TodoList = () => {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
+  console.log('todo: ' ,state);
+  useEffect(() => {
+    dispatch(getTodos())
+  }, [])
+
+
+  const renderCard = () => {
+    return (
+    <Card>
+      <CardContent>
+        <Typography variant="body2" component="p">
+          well meaning and kindly.
+          <br />
+          {'Test text'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Complete</Button>
+        <Button size="small">Delete</Button>
+      </CardActions>
+    </Card>
+    );
+  }
   return (
     <>
       <div>
-        Todo List
+        { renderCard() }
       </div>
     </>
   )
